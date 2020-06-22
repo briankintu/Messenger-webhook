abfc8a323801bd03b85cefbcfe974ecef96b9be8
Brian Richard Kintu <brian2xa@gmail.com>
2020-06-22 20:31:17 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 'use strict';
-
+const PAGE_ACCESS_TOKEN= process.env.PAGE_ACCESS_TOKEN;
 // Imports dependencies and set up http server
-const
+const 
+  request = require('request'),
   express = require('express'),
   bodyParser = require('body-parser'),
   app = express().use(bodyParser.json()); // creates express http server
+
 
   // Creates the endpoint for our webhook 
 app.post('/webhook', (req, res) => {  
@@ -21,6 +23,19 @@ app.post('/webhook', (req, res) => {
         // will only ever contain one message, so we get index 0
         let webhook_event = entry.messaging[0];
         console.log(webhook_event);
+
+        // Get the sender PSID
+        let sender_psid = webhook_event.sender.id;
+        console.log('Sender PSID: ' + sender_psid);
+
+        // Check if the event is a message or postback and
+       // pass the event to the appropriate handler function
+      if (webhook_event.message) {
+        handleMessage(sender_psid, webhook_event.message);        
+  }   else if (webhook_event.postback) {
+        handlePostback(sender_psid, webhook_event.postback);
+  }
+
       });
   
       // Returns a '200 OK' response to all requests
@@ -35,9 +50,8 @@ app.post('/webhook', (req, res) => {
   // Adds support for GET requests to our webhook
 app.get('/webhook', (req, res) => {
 
-    // Your verify token. Should be a random string.
-    let VERIFY_TOKEN = "workstuff"
-      
+  const VERIFY_TOKEN= "workstuff";
+
     // Parse the query params
     let mode = req.query['hub.mode'];
     let token = req.query['hub.verify_token'];
@@ -59,6 +73,58 @@ app.get('/webhook', (req, res) => {
       }
     }
   });
+
+  // Handles messages events
+function handleMessage(sender_psid, received_message) {
+  
+  let response;
+
+  // Check if the message contains text
+  if (received_message.text) {    
+
+    // Create the payload for a basic text message
+    response = {
+      "text": `You sent the message: "${received_message.text}". Now send me an image!`
+    }
+  }  
+  
+  // Sends the response message
+  callSendAPI(sender_psid, response);
+
+}
+
+// Handles messaging_postbacks events
+function handlePostback(sender_psid, received_postback) {
+
+}
+
+// Sends response messages via the Send API
+function callSendAPI(sender_psid, response) {
+
+  // Construct the message body
+  let request_body = {
+    "recipient": {
+      "id": sender_psid
+    },
+    "message": response
+  }
+
+  // Send the HTTP request to the Messenger Platform
+  request({
+    "uri": "https://graph.facebook.com/v7.0/me/messages",
+    "qs": { "EAAEh3cWZBgUgBAPNaakMEjIG49W6siIuy8NoTvTGdx1FNmeC9jD9Kgiz6wmP78uaLz0QupN1nZB8cvWoplfJ3o1xUU4tDN0ft6oJMZBap8oxZCnqk7osIbptsgwnNe8xvFZCTQFwhN1gB6Q8LzG8RAMeJYyNbjTvKsZANcjvkZBxbO9nvXAxejB": process.env.PAGE_ACCESS_TOKEN },
+    "method": "POST",
+    "json": request_body
+  }, (err, res, body) => {
+    if (!err) {
+      console.log('message sent!')
+    } else {
+      console.error("Unable to send message:" + err);
+    }
+  }); 
+
+  }
+  
 
 // Sets server port and logs message on success
 app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
